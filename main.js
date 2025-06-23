61d9454c932dd4a4ab542e0cce5fafb140cc07a6
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2025-06-24 04:22:57 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//bug with hover text on mobile
-//bug with cookies
-
-
 nimi_pi_sitelen_lasina = ["a", "akesi","ala","alasa","ale","anpa","ante","anu","awen","e","en","esun","ijo","ike","ilo","insa","jaki","jan","jelo","jo","kala","kalama","kama","kasi","ken","kepeken","kili","kiwen","ko","kon","kule","kulupu","kute","la","lape","laso","lawa","len","lete","li","lili","linja","lipu","loje","lon","luka","lukin","lupa","ma","mama","mani","meli","mi","mije","moku","moli", "monsi","mu","mun","musi","mute","nanpa","nasa","nasin","nena","ni","nimi","noka","o","olin","ona","open","pakala","pali","palisa","pan","pana","pi","pilin","pimeja","pini","pipi","poka","poki","pona","pu","sama","seli","selo","seme","sewi","sijelo","sike","sin","sina","sinpin","sitelen","sona","soweli","suli","suno","supa","suwi","tan","taso","tawa","telo","tenpo","toki","tomo","tonsi","tu","unpa","uta","utala","walo","wan","waso","wawa","weka","wile","*"]
 nimi_pi_sitelen_pona = ["󱤀", "󱤁","󱤂","󱤃","󱤄","󱤅","󱤆","󱤇","󱤈","󱤉","󱤊","󱤋","󱤌","󱤍","󱤎","󱤏","󱤐","󱤑","󱤒","󱤓","󱤔","󱤕","󱤖","󱤗","󱤘","󱤙","󱤚","󱤛","󱤜","󱤝","󱤞","󱤟","󱤠","󱤡","󱤢","󱤣","󱤤","󱤥","󱤦","󱤧","󱤨","󱤩","󱤪","󱤫","󱤬","󱤭","󱤮","󱤯","󱤰","󱤱","󱤲","󱤳","󱤴","󱤵","󱤶","󱤷","󱤸","󱤹","󱤺","󱤻","󱤼","󱤽","󱤾","󱤿","󱥀","󱥁","󱥂","󱥃","󱥄","󱥅","󱥆","󱥇","󱥈","󱥉","󱥊","󱥋","󱥌","󱥍","󱥎","󱥏","󱥐","󱥑","󱥒","󱥓","󱥔","󱥕","󱥖","󱥗","󱥘","󱥙","󱥚","󱥛","󱥜","󱥝","󱥞","󱥟","󱥠","󱥡","󱥢","󱥣","󱥤","󱥥","󱥦","󱥧","󱥨","󱥩","󱥪","󱥫","󱥬","󱥭","󱥾","󱥮","󱥯","󱥰","󱥱","󱥲","󱥳","󱥴","󱥵","󱥶","󱥷","󱦜"]
 points = 0
